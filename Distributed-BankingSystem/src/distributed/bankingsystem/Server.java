974fa7ca32bcf9e3c55dc8a374bdd5aae330a481
NourhanEssam <nourhanessam94@gmail.com>
2016-11-24 19:26:37 +0200

    Client and Server Classes added

--- Distributed-BankingSystem/src/distributed/bankingsystem/Server.java
+++ Distributed-BankingSystem/src/distributed/bankingsystem/Server.java
@@ -9,12 +9,12 @@ package distributed.bankingsystem;
  *
  * @author Essam
  */
-public class BankingSystem extends javax.swing.JFrame {
+public class Server extends javax.swing.JFrame {
 
     /**
-     * Creates new form BankingSystem
+     * Creates new form Server
      */
-    public BankingSystem() {
+    public Server() {
         initComponents();
     }
 
@@ -60,20 +60,20 @@ public class BankingSystem extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(BankingSystem.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Server.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(BankingSystem.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Server.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(BankingSystem.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Server.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(BankingSystem.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Server.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new BankingSystem().setVisible(true);
+                new Server().setVisible(true);
             }
         });
     }
